9072d9fc858728fd7784a2d2cec53e0fb966670b
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-07-30 16:53:55 +0200

    Embed slide

--- theme/src/components/body.js
+++ theme/src/components/body.js
@@ -2,6 +2,9 @@ import React from "react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import { Intro, Content } from "./placeholders"
 import splitSlides from "gatsby-theme-mdx-deck/src/split-slides"
+import Slide from "gatsby-theme-mdx-deck/src/components/slide"
+import Zoom from "gatsby-theme-mdx-deck/src/components/zoom"
+import { ThemeProvider } from "theme-ui"
 
 function getIntro(children) {
   const introElement = React.Children.toArray(children).find(
@@ -17,20 +20,31 @@ function getContent(children) {
   return contentElement && contentElement.props && contentElement.props.children
 }
 
-const Wrapper = ({ children }) => {
+const Wrapper = ({ children, theme = {}, ...props }) => {
+  console.log({ props })
   const intro = getIntro(children)
   const slides = splitSlides({ children })
-  const contents = slides.map(getContent)
-  const body = [intro]
+  // const contents = slides.map(getContent)
+  const kids = [intro]
   slides.forEach((slide, i) => {
-    body.push(slide)
-    body.push(contents[i])
+    kids.push(
+      <ThemeProvider theme={theme} key={i}>
+        <Zoom zoom={1} ratio={16 / 9}>
+          <Slide slide={slide} preview />
+        </Zoom>
+      </ThemeProvider>
+    )
+    kids.push(getContent(slide))
   })
-  return body
+  return kids
 }
 
 const Body = ({ body }) => {
-  return <MDXRenderer components={{ wrapper: Wrapper }}>{body}</MDXRenderer>
+  return (
+    <MDXRenderer components={{ wrapper: Wrapper }} body={body}>
+      {body}
+    </MDXRenderer>
+  )
 }
 
 export default Body
